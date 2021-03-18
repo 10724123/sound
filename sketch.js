1cf0d5cab135de3fbb4814353c6d83d929b710bf
10724123 <80090497+10724123@users.noreply.github.com>
2021-03-19 00:40:09 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://10724123.github.io/sound/model.json';
 
 
 function preload() {
